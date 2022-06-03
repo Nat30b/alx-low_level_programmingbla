9260e124c7861c4bba8dc113d4d610833a9facd3
Nat30b <natnaelbirhan30@gmail.com>
2022-06-03 12:07:58 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * * main -
- * * Return: Always 0 (Success)
- * */
+* * main -
+* * Return: Always 0 (Success)
+*/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
