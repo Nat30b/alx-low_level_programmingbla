bc7833413e68baf3668feafd9021296566b46738
Nat30b <natnaelbirhan30@gmail.com>
2022-06-03 10:57:31 -0700

    task44

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /*
- *Description: main - Prints out a string to stdout.
- *Return: 0 if success.
+ *main - Entry point
+ *
+ *Return: always 0
  */
 int main(void)
+	/*Main entry point*/
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
