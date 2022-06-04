7b68d054b22cc743731328546f445ff6ff8fa114
Nat30b <Natnaelbirhan30@gmail.com>
2022-06-04 11:23:11 -0700

    Task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,14 +5,19 @@
  *Return: Always 0 (Success)
  */
 
-int main(void){
+int main(void)
+{
 int ones = '0';
 int tens = '0';
 int hundreds = '0';
-for (hundreds = '0'; hundreds <= '9'; hundreds++){
-for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/{
-for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/{
-if (!((ones == tens) || (hundreds == tens) || (tens > ones) || (hundreds > tens)))/*eliminates repeatition*/{
+for (hundreds = '0'; hundreds <= '9'; hundreds++)
+{
+for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+{
+for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+{
+if (!((ones == tens) || (hundreds == tens) || (tens > ones) || (hundreds > tens)))/*eliminates repeatition*/
+{
 putchar(hundreds);
 putchar(tens);
 putchar(ones);
