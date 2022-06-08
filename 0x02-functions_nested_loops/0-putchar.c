4becfed2a2e861e49b8e6f995a8bfe498cbca5ba
Nat30b <Natnaelbirhan30@gmail.com>
2022-06-08 09:54:20 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- *main - function prints text as output
- *
- *Return:return 0
+ *main - Entry point
+ *   
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-		char school[10] = "_putchar";
+char *sh = "Holberton";
 
-			int i;
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
 
-				for (i = 0; i < 8; i++)
-						{
-									_putchar(school[i]);
-										}
-					_putchar(10);
-						return (0);
+return (0);
 }
