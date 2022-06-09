8683ed642755dbedd81f4434dc40b983c85756bd
Nat30b <Natnaelbirhan30@gmail.com>
2022-06-08 23:10:21 -0700

    Taskkk 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include <stdio.h>
 /**
  *main - causes an infinite loop
  *  
@@ -7,11 +7,8 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-
 /*
 *while (i < 10)
 *{
