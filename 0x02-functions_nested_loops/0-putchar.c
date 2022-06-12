f4aba06e45102fa6bef7c085d953f5f8cf9d23cc
Nat30b <Natnaelbirhan@gmail.com>
2022-06-12 12:47:48 -0700

    Task O

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * print alphabet -> prints the lowercase alphabet
+ */
+
+void print_alphabet(void)
+{
+	int j;
+	for(j= "a"; j<= "z"; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
