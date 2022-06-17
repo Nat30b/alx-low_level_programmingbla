bc19f5b55c495fe5b00e314fcbba2e6534b93d93
Nat30b <Natnaelbirhan@gmail.com>
2022-06-17 08:02:46 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include"main.h"
-
 /**
  *_isupper->this isafunction to check upper case alphas
  *@c:an integer paramer
  *Return:something but success
  */
-int isupper(intc)
+int _isupper(int c)
 {
-	    return (c > = 'A' &&  c < = 'Z');
+	    return ( c >= 'A' && c <='Z');
 }
