2adecf9df4ad998574320d88f4341fd175ad2e08
Nat30b <Natnaelbirhan@gmail.com>
2022-06-25 11:06:42 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
-include "main.h"
+#include "main.h"
 
 /**
- * print alphabet -> prints the lowercase alphabet
+ * main - prints putchar
+ * Return: always 0
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	int j;
-	for(j= "a"; j<= "z"; j++)
-	{
-		_putchar(j);
-	}
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-C function nested loops
+
