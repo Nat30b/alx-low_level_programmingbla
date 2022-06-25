66dc432bd8bdfc00ad1b7857f65a6866ac7ef248
Nat30b <Natnaelbirhan@gmail.com>
2022-06-25 12:46:19 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,6 +21,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 } else if (k >= 10)
+{
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
@@ -28,5 +29,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
 }
 }
