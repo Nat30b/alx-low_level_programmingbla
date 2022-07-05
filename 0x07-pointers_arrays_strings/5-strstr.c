ee12e27f80bb19767f0a36587253bef741489c49
Nat30b <Natnaelbirhan@gmail.com>
2022-07-05 04:26:18 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,14 +15,14 @@ while (*haystack != '\0')
 {
 str1 = haystack; /*values*/
 str2 = needle;
+
 /*Star WHILE*/
 while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 {
 haystack++;
 str2++;
 }
-if (*str2 == '\0')
-																	return (str1);
+if (*str2 == '\0')															return (str1);
 haystack = str1 + 1;															}
 return (0);
 }
